cf08bca395e96d266858882c7ba98073f03a1e26
Alexandre Janvrin <alexandre.janvrin@reseau.eseo.fr>
2021-05-02 10:48:05 +0200

    Don't know what changed (sorry)

--- headerexposer.py
+++ headerexposer.py
@@ -2,8 +2,10 @@
 # -*- coding: utf-8 -*-
 import re
 import sys
+
 sys.path.insert(0, "./")
 from headerexposer.__main__ import main
-if __name__ == '__main__':
-    sys.argv[0] = re.sub(r'(-script\.pyw|\.exe)?$', '', sys.argv[0])
+
+if __name__ == "__main__":
+    sys.argv[0] = re.sub(r"(-script\.pyw|\.exe)?$", "", sys.argv[0])
     sys.exit(main())
